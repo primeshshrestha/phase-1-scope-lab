49245e7dc090b2c0e30a4b42290c04f0b487647a
Primesh Shrestha <shresthaprimesh@gmail.com>
2023-02-02 11:05:41 -0700

    myscope lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'bob';
+
+function upperCaseCustomerName() {
+   return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+ bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newFavoriteCustomer) {
+  bestCustomer = newFavoriteCustomer;
+}
+
+function setnewfavorite(Customer) {
+  newFavoriteCustomer ="maybe bob";
+
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bob"
+}
